0815c6db770aa8a159dd8731b45e46a16d089788
Vaibhav-2211 <mr.vaibhavkhandelwal2211@gmail.com>
2026-02-08 23:56:29 +0530

    Add files via upload

---
+++ linkedlist insertion.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node (int value){
+        data = value;
+        next = NULL;
+
+    }
+};
+int main (){
+    int arr[5] = {1,2,3,4,5};
+
+Node *Head;
+Head = NULL;
+// cout<<Head->data<<endl;
+// cout<<Head->next<<endl;
+for (int i=0; i<5;i++){
+if (Head == NULL){
+    Head = new Node(arr[i]);
+
+}
+else {
+    Node *temp;
+    temp = new Node(arr[i]);
+    temp -> next = Head;
+    Head = temp;
+
+
+}}
+
+Node *temp = Head;
+while(temp!= NULL){
+cout<<temp -> data;
+temp = temp->next;
+}}
