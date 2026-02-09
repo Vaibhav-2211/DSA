a3ded1294cce472deaede7e160cadaa1ea4968c9
Vaibhav-2211 <mr.vaibhavkhandelwal2211@gmail.com>
2026-02-09 12:10:23 +0530

    Add files via upload

---
+++ insertionbypointertail.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node (int value){
+        data = value;
+        next = NULL;
+
+    }
+};
+int main (){
+    int arr[5] = {1,2,3,4,5};
+
+Node *Head;
+Node* Tail;
+Head = NULL;
+Tail = NULL;
+// cout<<Head->data<<endl;
+// cout<<Head->next<<endl;
+
+for (int i=0; i<5;i++){
+if (Head == NULL){
+    Head = new Node(arr[i]);
+Tail = Head;
+}
+else{
+   
+         Tail->next = new Node(arr[i]);
+         Tail = Tail->next;
+    }
+ 
+    }
+    Node *temp = Head;
+while(temp != NULL){
+cout<<temp -> data;
+temp = temp->next;
+}}
